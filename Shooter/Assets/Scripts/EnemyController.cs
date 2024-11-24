bb3cf4af556f32d993ed8c5ed1c0dd947bd1e1ee
Sergiusz Szwarc <sergiusz.sz@wp.pl>
2024-11-24 11:17:33 +0100

    Player Movement

---
+++ Shooter/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+     
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+
+
+    }
+
+
+}
